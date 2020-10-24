d3725cc7b28eb8b56490588225d7877b021b7893
Kai Ma <ksqsf@mail.ustc.edu.cn>
2020-10-24 16:33:48 +0800

    [lab2] fix test_syntax.sh again

--- tests/lab2/test_syntax.sh
+++ tests/lab2/test_syntax.sh
@@ -3,7 +3,7 @@
 # DO NOT MODIFY!
 # If you need customized behavior, please create your own script.
 
-CUR_DIR="$(dirname $(realpath $0))"
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TESTCASE="${1:-testcase}"
 TESTCASE_DIR="$CUR_DIR/$TESTCASE"
 OUTPUT_DIR="$CUR_DIR/syntree_$TESTCASE"
@@ -13,6 +13,8 @@ BUILD_DIR="$CUR_DIR/../../build"
 mkdir -p "$OUTPUT_DIR"
 
 # Generate a .syntax_tree file for all .cminus files in the testcase dir.
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 for testcase in "$TESTCASE_DIR"/*.cminus
 do
     filename="$(basename $testcase)"
@@ -32,3 +34,5 @@ if [[ ${2:-no} != "no" ]]; then
         echo "[info] No difference! Congratulations!"
     fi
 fi
+
+IFS=$SAVEIFS
